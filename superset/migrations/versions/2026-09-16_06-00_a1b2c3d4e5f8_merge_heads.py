e308e862e1c135b34a2da15d3a9e2f6e5bac1dfb
Kennedy Odhiambo <kennedy@pesapal.com>
2026-09-16 06:10:16 +0300

    chore(migrations): merge alembic heads into a1b2c3d4e5f8

---
+++ superset/migrations/versions/2026-09-16_06-00_a1b2c3d4e5f8_merge_heads.py
@@ -0,0 +1,35 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""merge heads
+
+Revision ID: a1b2c3d4e5f8
+Revises: ('c1a2b3d4e5f7', 'c7f53d184ea2', '88a01c781622')
+Create Date: 2026-09-16 06:00:00.000000
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = "a1b2c3d4e5f8"
+down_revision = ("c1a2b3d4e5f7", "c7f53d184ea2", "88a01c781622")
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
